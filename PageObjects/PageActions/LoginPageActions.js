dcf59b14c5cb05348b4286b0c56afadfb8bcdc25
Bidur Neupane <nbidur231@gmail.com>
2025-08-15 00:00:01 +0545

    Login POM second try

--- PageObjects/PageActions/LoginPageActions.js
+++ PageObjects/PageActions/LoginPageActions.js
@@ -3,7 +3,23 @@ export class LoginPageElements{
 
     username_text='[type="text"]'
     email_text='.signup-form > form > [type="email'
+    password_text='#password'
     signup_button='.signup-form > form > .btn'
+    gender_button='#id_gender1'
+    days_button='#days'
+    Months_button='#months'
+    years_button='#years'
+    newsletter_button='#newsletter'
+    firstname_text='#first_name'
+    lastname_text='#last_name'
+    company_text='#company'
+    country_button='#country'
+    state_text='#state'
+    city_text='#city'
+    zipcode_text='#zipcode'
+    mobile_text='#mobile_number'
+    submit_button='.login-form > form > .btn'
+
 
     Username(username){
          cy.get(this.username_text).type(username)
@@ -16,5 +32,19 @@ export class LoginPageElements{
     SignupButton(){
          cy.get(this.signup_button).click()
     }
+
+    Password(){
+     cy.get(this.password_text).type(1234)
+    }
+
+    FirstName(){
+     cy.get(this.firstname_text).type(Bidur)
+    }
+    
+    LastName(){
+     cy.get(this.lastname_text).type(Neupane)
+    }
+
+
 
 }
